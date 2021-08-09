01dca11d5092a743f60a65d3cd055ca65a428c63
Cesar Montalvo <cmontalvo360@users.noreply.github.com>
2021-08-09 20:12:13 +0000

    ch01 added

---
+++ ch01.py
@@ -0,0 +1,19 @@
+#NDE CHALLENGE #1 - Print
+
+#Set the following variables
+
+a="little tough to create. Especially"
+b="ones that are mixed up."
+c="Sentences are sometimes a"
+
+#Use the print function to print the following.
+
+#Sentences are sometimes a @@@
+print(c, end=" @@@ \n")
+#little tough to create. Especially @@@
+print(a, end=" @@@ \n")
+#ones that are mixed up.
+print(b)
+
+#Use ONLY the variables (a, b, c) as the *objects that get printed
+#Use both the sep= and end= to accomplish this task.
