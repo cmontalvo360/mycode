b9902fa85179b00724e8afbd4c10afb5176a0b0c
Cesar Montalvo <cmontalvo360@users.noreply.github.com>
2021-08-09 21:01:10 +0000

    learning about input statement

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+user_input = input("Please enter an IPv4 IP address:")
+
+print("You told me the IPv4 address is:", user_input)
+
+
+userI = input("What is the vendor name associated with the device:")
+print("You told me the IPv4 address is:", user_input)
