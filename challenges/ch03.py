deea2561973886bdb8cec7b80f7c6c081300bc82
Cesar Montalvo <cmontalvo360@users.noreply.github.com>
2021-08-10 16:55:21 +0000

    challenge 3

---
+++ challenges/ch03.py
@@ -0,0 +1,11 @@
+# NDE Challenge #3 - Dictionaries
+
+# Use the following dictionary
+
+pets = {"cats": ["fluffy", "snowball", "garfield"], "dogs": ["toto", "kujo", "spot"]}
+
+# print out the following sentence - NOTE - {} are to show WHAT should be formatted in
+
+# I have the {cats} {fluffy}, {snowball}, and {garfield}, and the {dogs} {toto}, {kujo}, and {spot}.
+
+print(f"I have the {pets.keys()} {pets["cats"][0]}, {pets["cats"][1]}, and {pets["cats"][2]}, and the pets.keys() {pets["dogs"][0]}, {pets["dogs"][1]}, and {pets["dogs"][2]}.")
