c96ca19d83c2d22322044358dfcb0a3615c2502e
Cesar Montalvo <cmontalvo360@users.noreply.github.com>
2021-08-10 21:46:04 +0000

    working on if logic

---
+++ iftest2/ipstatic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# prompts user for ip address
+ipchk = input("Apply an IP address: ")
+
+# a string tests as true
+if ipchk:
+    print("Looks like the IP address was set: " + ipchk)
